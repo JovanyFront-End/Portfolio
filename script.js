31d1cd132cc495e67af924ccaf7179ff2fc3b1ea
jovany <jovanydevstudio@gmail.com>
2026-09-04 15:41:56 -0700

    fixing responsive bugs andd added a few more projects with deleting older ones

--- script.js
+++ script.js
@@ -6,28 +6,28 @@ const myProjects = [
         link: "https://jovanyfront-end.github.io/GameHub/"
     },
     {
-        title: "E-commerce Website",
+        title: "Unique Crafts E-commerce Website",
         description: "E-commerce website built with HTML, TAILWINDcss, and JavaScript. It includes features like product listings, shopping cart, search functionality, and more!",
-        image: "istockphoto-2229745461-612x612.webp",
+        image: "download.jpg",
         link: "https://uniquecrafts.vercel.app/"
     },
     {
-        title: "Brand Showcase Website",
-        description: "Brand showcase website built with HTML, TAILWINDcss, and JavaScript. It features a responsive design and interactive elements to enhance user experience.",
+        title: "Peak Burger Restaurant Website",
+        description: "Restaurant website built with HTML, TAILWINDcss, and JavaScript. It features a responsive design and interactive elements to enhance user experience.",
         image: "https://scontent.fcai19-2.fna.fbcdn.net/v/t39.30808-6/346598715_904840570590745_5559111727381141541_n.jpg?stp=dst-jpg_tt6&cstp=mx1080x1080&ctp=s1080x1080&_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=c8Y0DhibzEYQ7kNvwH38KKn&_nc_oc=AdqMv842a3qH---9iJcgjNqQF6tWRNftAnrdaTRmw1rvKXGp5RvyLqaJd83ZZLAGxj4&_nc_zt=23&_nc_ht=scontent.fcai19-2.fna&_nc_gid=Vc2-NGL0bLdwpQbnse4fbg&_nc_ss=7b2a8&oh=00_AQI_EBogkiv4fTsOfC_r5BDgAggaWVvHVROQRKrlut7DCg&oe=6A9BAE62",
         link: "https://peak-burger-restaurant.vercel.app/"
     },
     {
-        title: "Landing Page Design",
-        description: "A sleek and modern website designed to showcase the latest MacBook Neo. Built with HTML and CSS, it features a responsive design and UI elements to enhance user experience.",
-        image: "alex-brinkman-d4uPQo9EPE8-unsplash.jpg",
-        link: "https://jovanyfront-end.github.io/MACBOOK-NEO/"
+        title: "Tofi's Restaurant Website",
+        description: "A sleek and modern website designed to showcase Tofi's restaurant. Built with HTML, CSS, and JavaScript, it features a responsive design and UI elements to enhance user experience.",
+        image: "486505443_122094365618825966_7385656639332978431_n.jpg",
+        link: "https://tofis.vercel.app/"
     },
     {
-        title: "ToDO List Website",
-        description: "A simple and modern design for a ToDo List. Built with HTML, CSS AND JAVASCRIPT, it features a responsive design and Add tasks functionality",
-        image: "eden-constantino-iJg1YzsEfqo-unsplash.jpg",
-        link: "https://jovanyfront-end.github.io/ToDo/"
+        title: "Crazy Burger Restaurant Website",
+        description: "Restaurant website built with HTML, TAILWINDcss, and JavaScript. It features a responsive design and interactive elements to enhance user experience.",
+        image: "636980032_122238919424042849_2690906732451393425_n.jpg",
+        link: "https://crazy-burger-beryl.vercel.app/"
     },
         {
         title: "Dashboard Website",
